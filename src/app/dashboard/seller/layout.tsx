2143e41052f33b3eafb8298ae3281bcf90ed2994
isaiah peoples <isaiahcpeoples@outlook.com>
2025-01-28 08:38:17 -0600

    Created Seller Dashboard/Store

--- src/app/dashboard/seller/layout.tsx
+++ src/app/dashboard/seller/layout.tsx
@@ -1,15 +1,13 @@
-import { currentUser } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-import { ReactNode } from "react";
-
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { ReactNode } from 'react'
 export default async function SellerDashboardLayout({
   children,
 }: {
-  children: ReactNode;
+  children: ReactNode
 }) {
   // Block non sellers from accessing the seller dashboard
-  const user = await currentUser();
-
-  if (user?.privateMetadata.role !== "SELLER") redirect("/");
-  return <div>{children}</div>;
+  const user = await currentUser()
+  if (user?.privateMetadata.role !== 'SELLER') redirect('/')
+  return <div>{children}</div>
 }
--- src/constants/data.ts
+++ src/constants/data.ts
@@ -38,3 +38,40 @@ export const adminDashboardSidebarOptions: DashboardSidebarMenuInterface[] = [
   },
 ];
 
+export const SellerDashboardSidebarOptions: DashboardSidebarMenuInterface[] = [
+  {
+    label: 'Dashboard',
+    icon: 'dashboard',
+    link: '',
+  },
+  {
+    label: 'Products',
+    icon: 'products',
+    link: 'products',
+  },
+  {
+    label: 'Orders',
+    icon: 'box-list',
+    link: 'orders',
+  },
+  {
+    label: 'Inventory',
+    icon: 'inventory',
+    link: 'inventory',
+  },
+  {
+    label: 'Coupons',
+    icon: 'coupon',
+    link: 'coupons',
+  },
+  {
+    label: 'Shipping',
+    icon: 'shipping',
+    link: 'shipping',
+  },
+  {
+    label: 'Settings',
+    icon: 'settings',
+    link: 'settings',
+  },
+]

4491c9334354a0811b6123d3dbd2c9c1fb847e1e
isaiah peoples <isaiahcpeoples@outlook.com>
2025-01-29 11:04:15 -0600

    Seller Dashboard and Products Table

---
+++ types/colorthief.d.ts
@@ -0,0 +1,15 @@
+// /types/colorthief.d.ts
+declare module 'colorthief' {
+  interface ColorThief {
+    getColor(image: HTMLImageElement | string, quality?: number): number[]
+    getPalette(
+      image: HTMLImageElement | string,
+      colorCount?: number,
+      quality?: number
+    ): number[][]
+  }
+  const ColorThief: {
+    new (): ColorThief
+  }
+  export default ColorThief
+}

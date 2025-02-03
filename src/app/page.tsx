39d3e4df7992905a9c8167dbd59ee122ec01c66f
isaiah peoples <isaiahcpeoples@outlook.com>
2025-02-03 14:51:55 -0600

    Added Offer Tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import ThemeToggle from "@/components/shared/theme-toggle";
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="p-5">
-      <div className="w-100 flex gap-x-5 justify-end">
-        <UserButton />
-        <ThemeToggle />
-      </div>
-      <h1 className="fon-bold text-blue-500 font-barlow">Home Page</h1>
+    <div>
+      <UserButton />
     </div>
   )
 }
